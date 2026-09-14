7ef9d9650d39d001f974fabf10281e47bcbb1a7b
omidmahdavinik-creator <omidmahdavinik@gmail.com>
2026-09-14 21:32:29 +0330

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,32 @@
 plugins {
-    id("com.android.application") version "8.7.3" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.omid.game"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.omid.game"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
 }
